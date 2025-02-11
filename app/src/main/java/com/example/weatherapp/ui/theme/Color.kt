855dbc76a71b0d24f1db22d67bc074c912bd02ab
Екатерина Мингер <ekaterinaminger@MacBook-Pro-Ekaterina.local>
2025-02-11 15:26:23 +0600

    add colors

--- app/src/main/java/com/example/weatherapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/weatherapp/ui/theme/Color.kt
@@ -9,3 +9,22 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+//light theme
+val Iris = Color(0xFF6151C3)
+val VioletBlue = Color(0xFF8174CF)
+val MoodyBlue = Color(0xFF8174CF)
+val PrettyPosie = Color(0xFFC0B9E7)
+val ChineseSilver = Color(0xFFDFDCF3)
+val PlaceboPurple = Color(0xFFEFEEF9)
+val MaWhite = Color(0xFFF7F6FC)
+
+//dark theme
+val NarwhalGrey = Color(0xFF080713)
+val BlackMarlin = Color(0xFF393942)
+val CharcoalPlum = Color(0xFF6B6A71)
+val Downpour = Color(0xFF9C9CA1)
+val LightGray = Color(0xFFCECDD0)
+val MoonLily = Color(0xFFE6E6E7)
+val BleachedSilk = Color(0xFFF3F3F3)
+
